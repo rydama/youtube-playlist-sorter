e398c549b13c9641d25d1cd2b559453bea523af1
Ryan Manwiller <rydama@gmail.com>
2017-04-01 13:33:09 -0700

    Login/out buttons stubbed in

--- src/login-control.jsx
+++ src/login-control.jsx
@@ -1,17 +1,34 @@
 import React from "react";
 
-export default function LoginControl(props) {
-  if (props.loggedIn) {
-    return(
-      <p>
-        <button id="logout-button">Logout</button>
-      </p>
-    );
-  } else {
-    return(
-      <p>
-        <button id="login-button">Login with YouTube</button>
-      </p>
-    );
+export default class LoginControl extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { isLoggedIn: false }
+    this.handleLoginClicked = this.handleLoginClicked.bind(this)
+    this.handleLogoutClicked = this.handleLogoutClicked.bind(this)
+  }
+
+  handleLoginClicked() {
+    this.setState( { isLoggedIn: true} )
+  }
+
+  handleLogoutClicked() {
+    this.setState( { isLoggedIn: false} )
+  }
+
+  render() {
+    if (this.state.isLoggedIn) {
+      return(
+        <p>
+          <button id="logout-button" onClick={this.handleLogoutClicked}>Logout</button>
+        </p>
+      );
+    } else {
+      return(
+        <p>
+          <button id="login-button" onClick={this.handleLoginClicked}>Login with YouTube</button>
+        </p>
+      );
+    }
   }
 }
