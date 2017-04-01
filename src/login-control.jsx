ae601329d16847cd75c8b95d1119834c8dedfa1e
Ryan Manwiller <rydama@gmail.com>
2017-04-01 13:46:08 -0700

    Move loggedIn state up to app level

--- src/login-control.jsx
+++ src/login-control.jsx
@@ -3,21 +3,20 @@ import React from "react";
 export default class LoginControl extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { isLoggedIn: false }
     this.handleLoginClicked = this.handleLoginClicked.bind(this)
     this.handleLogoutClicked = this.handleLogoutClicked.bind(this)
   }
 
   handleLoginClicked() {
-    this.setState( { isLoggedIn: true} )
+    this.props.onLoginSuccess("TOKEN")
   }
 
   handleLogoutClicked() {
-    this.setState( { isLoggedIn: false} )
+    this.props.onLogout()
   }
 
   render() {
-    if (this.state.isLoggedIn) {
+    if (this.props.isLoggedIn) {
       return(
         <p>
           <button id="logout-button" onClick={this.handleLogoutClicked}>Logout</button>
--- src/playlist-manager.jsx
+++ src/playlist-manager.jsx
@@ -3,19 +3,33 @@ import LoginControl from "./login-control"
 
 export default class PlaylistManager extends React.Component {
   constructor() {
-    super();
+    super()
+
+    this.handleLoginSuccess = this.handleLoginSuccess.bind(this)
+    this.handleLogout = this.handleLogout.bind(this)
 
     this.state = {
       accessToken: null
-    };
+    }
+  }
+
+  handleLoginSuccess(accessToken) {
+    this.setState( { accessToken: accessToken} )
+  }
+
+  handleLogout() {
+    this.setState( { accessToken: null} )
   }
 
   render() {
     return(
       <div>
         <h1>YouTube Playlist Manager</h1>
-        <LoginControl loggedIn={!!this.state.accessToken} />
+        <LoginControl
+          isLoggedIn={!!this.state.accessToken}
+          onLoginSuccess={this.handleLoginSuccess}
+          onLogout={this.handleLogout} />
       </div>
-    );
+    )
   }
 }
